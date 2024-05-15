e3f91b4881d580ee1eb38de1b3931fe2a750087a
amfunNyLORD <funnybulletgames@yandex.ru>
2024-05-15 19:01:39 +0300

    lab7: добавил поворот камеры в morphing'е

--- lab7/morphing/CMyApplication.h
+++ lab7/morphing/CMyApplication.h
@@ -20,8 +20,28 @@ protected:
 	void OnDisplay() override;
 	void OnReshape(int width, int height) override;
 
+	virtual void OnMouse(int button, int state, int x, int y);
+	virtual void OnMotion(int x, int y);
+
 private:
 	void InitShaders();
+	
+	// ¬ращаем камеру вокруг начала кординат на заданный угол
+	static void RotateCamera(GLfloat rotateX, GLfloat rotateY);
+
+	// ѕроизводим ортонормирование матрицы моделировани€-вида
+	static void NormalizeModelViewMatrix(void);
+
+	// ‘лаг, свидетельствующий о состо€нии левой кнопки мыши
+	bool m_leftButtonPressed;
+
+	// —тарые координаты курсора мыши
+	int m_mouseX;
+	int m_mouseY;
+
+	int m_windowWidth;
+	int m_windowHeight;
+
 	CSurface m_surface;
 	CProgram m_program;
 };
